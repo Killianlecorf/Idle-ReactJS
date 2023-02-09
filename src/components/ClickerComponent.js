7b90644c258ebf28bb8a6d1edee8b15377f40423
killian-lecorf <killianlecorf@outlook.fr>
2023-02-09 16:55:15 +0100

    add miner Two

--- src/components/ClickerComponent.js
+++ src/components/ClickerComponent.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 const ClickerComponent = () => {
 
     const intervalId = useRef(null)
+    const intervalMinerTwoId = useRef(null)
     const [coins, setCoins] = useState(0)
     const [idleMiners, setdleMiners] = useState({
         miner: 0,
@@ -21,7 +22,7 @@ const ClickerComponent = () => {
         }
     }
 
-    const additionMiner = () => { // idleMiners.miner
+    const additionMiner = () => {
         setCoins((previousValue) => {
             return previousValue + idleMiners.miner
         })
@@ -32,16 +33,58 @@ const ClickerComponent = () => {
             clearInterval(intervalId.current)
             intervalId.current = setInterval(additionMiner, 1000)
         }
+        
     },[idleMiners.miner])
+
+
+
+
+
+    const handleBuyMinerTwo = () => {
+        if (coins >= 1) {
+            // setCoins(coins - 1000)
+            setdleMiners({...idleMiners, minerTwo: idleMiners.minerTwo + 1 })
+        }
+    }
+
+    const additionMinerTwo = () => {
+        setCoins((previousValue) => {
+            return previousValue + (idleMiners.minerTwo*10)
+        })
+    }
+
+    useEffect(() => {
+        if (idleMiners.minerTwo !== 0) {
+            clearInterval(intervalMinerTwoId.current)
+            intervalMinerTwoId.current = setInterval(additionMinerTwo, 1000)
+        }
+    },[idleMiners.miner])
+
 
     return (
         <div>
             <button onClick={ handleChangeCoins }>Click</button>
             <p>{ coins }</p>
-            <button onClick={ handleBuyMiner } >buy Miner</button>
+            <button onClick={ handleBuyMiner } >buy Miner for 10 coins</button>
             <p>mineur : { idleMiners.miner }</p>
+            <button onClick={ handleBuyMinerTwo } >buy Miner for 1000 coins</button>
+            <p>mineurTwo : { idleMiners.minerTwo }</p>
         </div>
     );
 };
 
 export default ClickerComponent;
+
+// const clickit = () => {
+//     const button = document.getElementById('button');
+//     const worker = document.getElementById('worker');
+//     for (let index = 0; index < 50; index++) {
+//         button.click()
+//         console.log(button)
+//     }
+
+//     document.addEventListener('mousemove', () => {
+//         button.click()
+//         worker.click()
+//     })
+// } 
